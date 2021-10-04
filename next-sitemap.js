fa8d280d91af1fe7e87aaa7f2cc5b87559f7cd75
ayumitk <ayumee528@gmail.com>
2021-10-04 01:32:41 -0700

    fix sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -5,5 +5,5 @@ module.exports = {
     policies: [{ userAgent: "*", allow: "/", disallow: "/admin/" }],
   },
   sitemapSize: 7000,
-  exclude: ["/admin/*", "/archives/*"],
+  exclude: ["/admin/*"],
 };
