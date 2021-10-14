6e7f784901f18a14f04aa69f26c2105ca4cd4588
ayumitk <ayumee528@gmail.com>
2021-10-14 16:46:07 -0700

    add reduce moment size

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+const MomentLocalesPlugin = require("moment-locales-webpack-plugin");
+const locales = new MomentLocalesPlugin({
+  localesToKeep: ["ja"],
+});
+const MomentTimezoneDataPlugin = require("moment-timezone-data-webpack-plugin");
+const currentYear = new Date().getFullYear();
+const timezone = new MomentTimezoneDataPlugin({
+  matchZones: "Asia/Tokyo",
+  startYear: currentYear - 5,
+  endYear: currentYear + 5,
+});
+
+module.exports = withBundleAnalyzer({
+  webpack: (config) => {
+    config.plugins.push(locales, timezone);
+    return config;
+  },
   images: {
     domains: [
       "m.media-amazon.com",
@@ -6,12 +26,7 @@ module.exports = {
       "img.papy.co.jp",
       "ebook-assets.dmm.com",
       "cmoa.akamaized.net",
-      "muchimemo.com"
+      "muchimemo.com",
     ],
   },
-};
-
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-module.exports = withBundleAnalyzer({})
+});
