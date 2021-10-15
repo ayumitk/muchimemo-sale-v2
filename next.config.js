c552a62a6727a10fa0edfe8e80340d21aa82869e
ayumitk <ayumee528@gmail.com>
2021-10-14 19:07:59 -0700

    update

--- next.config.js
+++ next.config.js
@@ -19,9 +19,6 @@ module.exports = withBundleAnalyzer({
     config.plugins.push(locales, timezone);
     return config;
   },
-  experimental: {
-    optimizeFonts: true,
-  },
   images: {
     domains: [
       "m.media-amazon.com",
